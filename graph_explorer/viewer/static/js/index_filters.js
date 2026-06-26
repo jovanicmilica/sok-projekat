e3d3622b8f7d3dc15059c0b1aebdd83d7399a9ed
dkomatovic <danicakomatovic@gmail.com>
2026-06-27 00:43:36 +0200

    #ISSUE-57 Minor fixes

--- graph_explorer/viewer/static/js/index_filters.js
+++ graph_explorer/viewer/static/js/index_filters.js
@@ -153,6 +153,9 @@ async function applyGraphOperations() {
     }
 
     graphOperations = data.operations || graphOperations;
+    if (typeof window.refreshWorkspacesFromPayload === 'function') {
+        window.refreshWorkspacesFromPayload(data);
+    }
     syncOperationLists();
     renderAppliedOperations();
 
@@ -188,6 +191,23 @@ async function removeOperation(index) {
         renderAppliedOperations();
         showFilterError(error.message);
     }
+}
+
+function setGraphOperationsFromServer(operations) {
+    graphOperations = Array.isArray(operations) ? operations.map(operation => ({
+        type: operation.type,
+        query: operation.query
+    })) : [];
+    syncOperationLists();
+    clearFilterError();
+    renderAppliedOperations();
+}
+
+function getGraphOperationsSnapshot() {
+    return graphOperations.map(operation => ({
+        type: operation.type,
+        query: operation.query
+    }));
 }
 
 function resetGraphOperations() {
@@ -297,6 +317,8 @@ function showCurrentState() {
 
 window.resetGraphOperations = resetGraphOperations;
 window.clearGraphOperationsForNewGraph = clearGraphOperationsForNewGraph;
+window.setGraphOperationsFromServer = setGraphOperationsFromServer;
+window.getGraphOperationsSnapshot = getGraphOperationsSnapshot;
 
 document.addEventListener('DOMContentLoaded', setupFilterInputs);
 document.addEventListener('DOMContentLoaded', setupViewChooser);
